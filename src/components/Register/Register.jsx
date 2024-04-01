cba7599a203a476faf6a93e4efe5eff75e084819
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2024-04-01 11:01:00 +0600

    creat terms and condition checkbox

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -14,7 +14,9 @@ const Register = () => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
-        console.log(email, password)
+        const accepted = e.target.terms.checked;
+
+        console.log(email, password, accepted)
 
         // reset error
         setRegisterError('');
@@ -27,6 +29,10 @@ const Register = () => {
         }
         else if (!/[A-Z]/.test(password)) {
             setRegisterError('Your password Should be have at least one upper case character')
+        }
+        else if (!accepted) {
+            setRegisterError("Please accept terms and condition")
+            return;
         }
 
 
@@ -55,13 +61,17 @@ const Register = () => {
                             name="password"
                             placeholder='Password'
                             id="password" required />
-                        <span  onClick={() => setShowPassword(!showPassword)} className='absolute top-3 right-2'>
+                        <span onClick={() => setShowPassword(!showPassword)} className='absolute top-3 right-2'>
                             {
                                 showPassword ? <IoMdEye /> : <IoMdEyeOff />
                             }
                         </span>
                     </div>
                     <br />
+                    <div className='mb-2'>
+                        <input type="checkbox" name="terms" id="terms" />
+                        <label className='ml-2 ' htmlFor="terms">Accept out <a href="">terms and condition</a></label>
+                    </div>
                     <input className='btn btn-secondary mb-4 w-full' type="submit" value="Register" />
                 </form>
                 {
